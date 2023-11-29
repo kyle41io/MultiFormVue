f3db6490fcefc0af186f6ce23d12641c51374582
kyle41io <vankien41hust@gmail.com>
2023-11-29 16:41:22 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import PersonalInfo from "@/components/PersonalInfo.vue";
+import WorkExperience from "@/components/WorkExperience.vue";
+import ConfirmInfo from "@/components/ConfirmInfo.vue";
+
+export default {
+  components: { PersonalInfo, WorkExperience, ConfirmInfo },
+  data() {
+    return {
+      component: PersonalInfo,
+    };
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <keep-alive><component :is="component"></component></keep-alive>
+  </div>
+</template>
+
+<style scoped></style>
